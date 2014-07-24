81a7d5c0761da816a3796215c929aece16a298d1
Pierre de Buyl <pdebuyl@ulb.ac.be>
2014-07-24 12:40:27 +0200

    add author and info in othertestfile

--- othertestfile.py
+++ othertestfile.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
+__author__ = "Pierre de Buyl"
+
+print "This is %s %s by %s" % (__file__, __version__, __author__)
 
 print("Other hello!")
