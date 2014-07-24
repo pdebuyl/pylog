a9894573d6c160ad4d8ab9a369348bac7ae3d2d0
Pierre de Buyl <pdebuyl@ulb.ac.be>
2014-07-24 12:22:01 +0200

    Testing with more difficulty

--- testfile.py
+++ testfile.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 __author__ = "Pierre de Buyl"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
+
+print "This is %s %s by %s" % (__file__, __version__, __author__)
 
 print("Hello, world!")
 print("Hello, world!")
