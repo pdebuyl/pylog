b8b620e0b9d92d6558bbcc4dc6d06316c1afe63d
Pierre de Buyl <pdebuyl@ulb.ac.be>
2014-07-24 12:46:24 +0200

    Add docstring.

--- testfile.py
+++ testfile.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+"""The program testfile is an automatically-version-numbered program. And
+that is quite cool :-)
+"""
+
 __author__ = "Pierre de Buyl"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 print "This is %s %s by %s" % (__file__, __version__, __author__)
 
