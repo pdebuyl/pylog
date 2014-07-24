50eb708b87fc7edd13471e293b5308f6db7317c0
Pierre de Buyl <pdebuyl@ulb.ac.be>
2014-07-24 16:25:18 +0200

    Remove test files.

--- hello.py
+++
@@ -1,5 +0,0 @@
-
-__version__ = 0.1.0
-
-print "Hello, world!"
-
--- othertestfile.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-__version__ = "0.0.1"
-__author__ = "Pierre de Buyl"
-
-print "This is %s %s by %s" % (__file__, __version__, __author__)
-
-print("Other hello!")
